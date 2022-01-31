3e7291e78d1bca192c2f6752b1818cd2bd338e5e
myrdinaptaliesen <nicog.dev@gmail.com>
2022-01-31 22:00:45 +0100

    Les formulaires

--- src/Form/ArticlesType.php
+++ src/Form/ArticlesType.php
@@ -6,13 +6,16 @@ use App\Entity\Articles;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ArticlesType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
+        ->add('title')->add('title', TextType::class, [
+            'label' => 'Notre titre'
+            ])           
             ->add('content')
         ;
     }
