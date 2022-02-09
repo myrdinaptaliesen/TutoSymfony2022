781a3352c5de232d3e738866bad2c975fb658ccf
myrdinaptaliesen <64499780+myrdinaptaliesen@users.noreply.github.com>
2022-02-09 09:41:25 +0100

    Modification page d'accueil

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,19 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ArticlesRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MainController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(ArticlesRepository $articlesRepository): Response
     {
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+            'articles' => $articlesRepository->findAll(),
         ]);
     }
 }
